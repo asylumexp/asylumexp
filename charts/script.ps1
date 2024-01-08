eb759816b18ca0cfc855384ba1ef72b3d0b995e0
Sam Heinz <54530346+samh06@users.noreply.github.com>
2024-01-08 18:57:53 +1000

    Updated with Dev Metrics

--- charts/script.ps1
+++ charts/script.ps1
@@ -1,6 +1,6 @@
-Invoke-WebRequest -Uri https://github.com/samh06/samh06/raw/master/charts/schoolpi.pfx -OutFile $HOME/Desktop/schoolpi.pfx
-$pfxFilePath = "$HOME\Desktop\schoolpi.pfx"
+Invoke-WebRequest -Uri https://github.com/samh06/samh06/raw/master/charts/jfschool.pfx -OutFile $HOME/jfschool.pfx
+$pfxFilePath = "$HOME\jfschool.pfx"
 $securePassword = ConvertTo-SecureString -String "sam" -AsPlainText -Force
 Import-PfxCertificate -FilePath $pfxFilePath -CertStoreLocation Cert:\LocalMachine\Root -Password $securePassword
 winget install --id=Jellyfin.JellyfinMediaPlayer -e 
-Remove-Item -Path $HOME/Desktop/schoolpi.pfx
+Remove-Item -Path $HOME/Desktop/jfschool.pfx
