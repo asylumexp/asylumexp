cf48d8a52d4acd64820c04d540d1629c2c425531
Sam Heinz <54530346+samh06@users.noreply.github.com>
2024-01-08 19:13:13 +1000

    Update with Dev Metrics

--- charts/script.ps1
+++ charts/script.ps1
@@ -1,6 +1,12 @@
-Invoke-WebRequest -Uri https://github.com/samh06/samh06/raw/master/charts/jfschool.pfx -OutFile $HOME/jfschool.pfx
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+Invoke-WebRequest -Uri https://github.com/samh06/samh06/raw/master/charts/schooljf.pfx -OutFile $HOME/jfschool.pfx
 $pfxFilePath = "$HOME\jfschool.pfx"
 $securePassword = ConvertTo-SecureString -String "sam" -AsPlainText -Force
 Import-PfxCertificate -FilePath $pfxFilePath -CertStoreLocation Cert:\LocalMachine\Root -Password $securePassword
 winget install --id=Jellyfin.JellyfinMediaPlayer -e 
-Remove-Item -Path $HOME/Desktop/jfschool.pfx
+Remove-Item -Path $HOME/jfschool.pfx
